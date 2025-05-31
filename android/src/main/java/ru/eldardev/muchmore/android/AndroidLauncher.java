d0379f4d9a80844f81b8ca98fc873bc9e9bbf250
Eldar <EldarK@outlook.com>
2025-05-31 10:17:35 +0300

    Gdx liftoff

---
+++ android/src/main/java/ru/eldardev/muchmore/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package ru.eldardev.muchmore.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.eldardev.muchmore.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
